0fbdb4d31de727685b8d695ed19ec29a78c36896
Toma Brasoveanu <104178438+Norok-The-Diablo@users.noreply.github.com>
2023-07-24 17:31:04 -0400

    Update Copy Config Linux.py

--- Copy Config Linux.py
+++ Copy Config Linux.py
@@ -1,7 +1,9 @@
 import subprocess
 username = subprocess.check_output(['whoami']).strip()
 user = username.decode()
-subprocess.run(["rm", "-rf", "/home/{user}/.config/nvim"])
+subprocess.run(["rm", "-rf", f"/home/{user}/.config/nvim"])
 subprocess.run(["cp", "-r", "nvim", f"/home/{user}/.config/"])
+subprocess.run(["rm", "-rf", f"/home/{user}/.local/share/nvim/plugged"])
+subprocess.run(["cp", "-r", "plugged", f"/home/{user}/.local/share/nvim/"])
 subprocess.run(["sudo", "mkdir", "/usr/local/share/fonts/truetype"])
 subprocess.run(["sudo", "cp", "-r", "font.ttf"], "/usr/share/fonts/truetype/")
