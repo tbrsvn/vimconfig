7348ec481e7473e97a1d3e5a7fc6e17da780f55b
Toma Brasoveanu <104178438+Norok-The-Diablo@users.noreply.github.com>
2023-07-23 23:47:54 -0400

    Update Copy Config Windows.bat

--- Copy Config Windows.bat
+++ Copy Config Windows.bat
@@ -1,3 +1,5 @@
 del C:\Users\%USERNAME%\AppData\Local\nvim
 mkdir C:\Users\%USERNAME%\AppData\Local\nvim
 copy nvim C:\Users\%USERNAME%\AppData\Local\nvim
+copy font.ttf %SYSTEMROOT%\Fonts\font.ttf
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Hack (TrueType)" /t REG_SZ /d font.ttf /f
