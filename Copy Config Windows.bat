c2be36a3b1ba2f03f6e970b21720b6b735c19eb9
Toma Brasoveanu <104178438+Norok-The-Diablo@users.noreply.github.com>
2023-07-28 18:46:06 -0400

    Update Copy Config Windows.bat

--- Copy Config Windows.bat
+++ Copy Config Windows.bat
@@ -4,5 +4,8 @@ copy nvim C:\Users\%USERNAME%\AppData\Local\nvim
 del C:\Users\%USERNAME%\AppData\Local\nvim-data\plugged
 mkdir C:\Users\%USERNAME%\AppData\Local\nvim-data\plugged
 copy plugged C:\Users\%USERNAME%\AppData\Local\nvim-data\plugged
+del C:\Users\%USERNAME%\vimfiles
+mkdir C:\Users\%USERNAME%\vimfiles
+copy .vim\* C:\Users\%USERNAME%\vimfiles
 copy font.ttf %SYSTEMROOT%\Fonts\nvimfont.ttf
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Hack For Neovim (TrueType)" /t REG_SZ /d nvimfont.ttf /f
