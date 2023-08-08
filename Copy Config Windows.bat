7728c1f7b3a7b0c5bb21a601b267a5f296383691
Toma Brasoveanu <104178438+Norok-The-Diablo@users.noreply.github.com>
2023-08-08 09:17:50 -0700

    Update Copy Config Windows.bat

--- Copy Config Windows.bat
+++ Copy Config Windows.bat
@@ -1,6 +1,8 @@
+mkdir C:\Users\%USERNAME%\AppData\Local\nvim
 copy nvim C:\Users\%USERNAME%\AppData\Local\nvim
 mkdir C:\Users\%USERNAME%\AppData\Local\nvim\autoload
 copy autoload C:\Users\%USERNAME%\AppData\Local\nvim\autoload
+mkdir C:\Users\%USERNAME%\vimfiles
 copy vimfiles C:\Users\%USERNAME%\vimfiles
 mkdir C:\Users\%USERNAME%\vimfiles\autoload
 copy autoload C:\Users\%USERNAME%\vimfiles\autoload
