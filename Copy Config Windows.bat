ed89f184ae1871ebed040c8f6b7174f4c26c0aaf
Toma Brasoveanu <104178438+Norok-The-Diablo@users.noreply.github.com>
2023-08-22 09:12:00 -0700

    Delete Copy Config Windows.bat

--- Copy Config Windows.bat
+++
@@ -1,9 +0,0 @@
-mkdir C:\Users\%USERNAME%\AppData\Local\nvim
-copy nvim C:\Users\%USERNAME%\AppData\Local\nvim
-mkdir C:\Users\%USERNAME%\AppData\Local\nvim\autoload
-copy autoload C:\Users\%USERNAME%\AppData\Local\nvim\autoload
-mkdir C:\Users\%USERNAME%\vimfiles
-copy vimfiles C:\Users\%USERNAME%\vimfiles
-mkdir C:\Users\%USERNAME%\vimfiles\autoload
-copy autoload C:\Users\%USERNAME%\vimfiles\autoload
-copy _vimrc C:\Users\%USERNAME%\_vimrc
