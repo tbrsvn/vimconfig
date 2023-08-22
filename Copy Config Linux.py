5bd7b37b3e623e160ce9f967c30c82c3dad19268
Toma Brasoveanu <104178438+Norok-The-Diablo@users.noreply.github.com>
2023-08-22 09:11:48 -0700

    Delete Copy Config Linux.py

--- Copy Config Linux.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-username = subprocess.check_output(['whoami']).strip()
-user = username.decode()
-subprocess.run(["rm", "-rf", f"/home/{user}/.config/nvim"])
-subprocess.run(["cp", "-r", "nvim", f"/home/{user}/.config/"])
-subprocess.run(["rm", "-rf", f"/home/{user}/.vim"])
-subprocess.run(["cp", "-r", ".vim", f"/home/{user}"])
-subprocess.run(["cp", "-r", ".vimrc", f"/home/{user}"])
-subprocess.run(["sudo", "cp", "-r", "nvimfont.ttf", "/usr/share/fonts/"])
